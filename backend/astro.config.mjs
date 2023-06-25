01a3a4a2807c2f663000e3bf1ee9bde3d21ca474
Pavitra Golchha <hello@pavi2410.me>
2023-06-25 22:33:30 +0530

    Made the landing page

--- backend/astro.config.mjs
+++ backend/astro.config.mjs
@@ -1,9 +1,27 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+import Icons from 'unplugin-icons/vite';
+import unocss from "@unocss/astro";
+import presetUno from '@unocss/preset-uno'
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [
+    unocss({
+      injectReset: true,
+      presets: [
+        presetUno(),
+      ]
+    })
+  ],
+  vite: {
+    plugins: [
+      Icons({
+        compiler: 'astro',
+        autoInstall: true
+      })
+    ]
+  }
 });
