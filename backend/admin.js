2e00f0cb28688c3a5bf383f359c669d3a302b52f
Pavitra Golchha <hello@pavi2410.me>
2023-10-28 19:09:55 +0530

    Firebase admin key

--- backend/admin.js
+++ backend/admin.js
@@ -1,9 +1,11 @@
 // Import the functions you need from the SDKs you need
-import {applicationDefault, initializeApp} from "firebase-admin/app";
+import {applicationDefault, cert, initializeApp} from "firebase-admin/app";
 import {getFirestore} from "firebase-admin/firestore";
+
+const FIREBASE_ADMIN_KEY = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
 
 // Initialize Firebase
 const app = initializeApp({
-    credential: applicationDefault(),
+    credential: cert(FIREBASE_ADMIN_KEY),
 });
 export const db = getFirestore(app);
--- backend/main.js
+++ backend/main.js
@@ -1,4 +1,4 @@
-import {db} from './db.js';
+import {db} from './admin.js';
 import {getProfile} from "./worker.js";
 
 export async function main() {
