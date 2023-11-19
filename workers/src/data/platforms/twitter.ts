38555e4dec6a34e8fab7ec0a176e8ef22923d99c
Pavitra Golchha <hello@pavi2410.me>
2023-11-19 16:31:18 +0530

    Fix twitter

--- workers/src/data/platforms/twitter.ts
+++ workers/src/data/platforms/twitter.ts
@@ -1,32 +1,26 @@
 import type { ProfileResult } from '.';
 
-export async function fetchTwitterProfile(username: string): Promise<ProfileResult['metrics'] | null> {
-	const options = {
-		method: 'POST',
+const REGEX = {
+	followers: /<li class="followers">.*?<\/span>.*?<span.*?>(.*?)<\/span>/s,
+};
+
+export async function fetchProfile(username: string): Promise<ProfileResult['metrics'] | null> {
+	const res = await fetch('https://n.biendeo.com/' + username, {
 		headers: {
-			'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
-			cookie:
-				'guest_id_marketing=v1%253A168709844043604425; guest_id_ads=v1%253A168709844043604425; personalization_id=%22v1_PFdcK%2F%2BUTsMB6QL41wa9uA%3D%3D%22; guest_id=v1%253A168709844043604425',
-			Authorization:
-				'Bearer AAAAAAAAAAAAAAAAAAAAAPYXBAAAAAAACLXUNDekMxqa8h%2F40K4moUkGsoc%3DTYfbDKbT3jJPCEVnMYqilB28NHfOPqkca3qaAxGfsyKCs0wRbw',
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0',
 		},
-	};
+	});
+	const html = await res.text();
 
-	const url = new URL('https://api.twitter.com/graphql/4S2ihIKfF3xhp-ENxvUAfQ/UserByScreenName');
+	const matches = html.match(REGEX.followers);
+	if (!matches) {
+		return null;
+	}
 
-	url.searchParams.set(
-		'variables',
-		JSON.stringify({
-			screen_name: username,
-			withHighlightedLabel: true,
-		})
-	);
-
-	const res = await fetch(url, options);
-	const json = await res.json();
+	const followers = parseInt(matches[1].replaceAll(',', '')) ?? -1;
 
 	return {
-		followers: json.data.user.legacy.followers_count,
+		followers,
 		subscribers: 0,
 	};
 }
